f5c35edf421090f4851ebbb8365ff403954b8692
ThiryS <thiry.simon1@gmail.com>
2020-06-25 09:35:42 +0200

    11 done

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -10,5 +10,37 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    async function xmensRecup(){
+        
+        let response = await fetch("http://localhost:3000/heroes");
+        let xmen = await response.json();
+        let addName = document.getElementById("hero-name").value
+        let addPowers = document.getElementById("hero-powers").value;
+        let alterEgo = document.getElementById("hero-alter-ego").value;
+
+        if( addName == "" || alterEgo == "" || addPowers == "" ){
+
+            alert("All fields are required")
+
+        } else {
+
+            newXmen = {
+            id : xmen.length + 1,
+            name : addName,
+            alterEgo : alterEgo,
+            abilities : addPowers.split(",")
+
+        }
+
+        xmen.push(newXmen);
+
+        console.table(xmen);
+        }
+    }
+
+    document.getElementById("run").addEventListener("click", ()=>{
+        
+        xmensRecup();
+
+    });
 })();
